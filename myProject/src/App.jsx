1759d6853b90dcb41b6d1381bfd3ceb3a5fc134a
malikharisnisar <malikmohdharis89@gmail.com>
2025-12-20 14:57:38 +0500

    first project

---
+++ myProject/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import {CounterButtons} from "./component/counter.jsx"
+import "./style/style.css"
+function App() {
+  return (
+    <>
+    <CounterButtons />
+    </>
+  )
+}
+
+export default App
